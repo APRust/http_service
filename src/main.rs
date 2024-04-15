db738bca8b7980a36612b4f909e36a4dc562849a
APRust <alex.pikalov.rust@gmail.com>
2024-04-15 17:52:16 +0300

    implementation with ThreadPool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use http_service::ThreadPool;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let http_request = BufReader::new(&mut stream).lines().next().unwrap().unwrap();
+    let (status, file_name) = match &http_request[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap()
 }
